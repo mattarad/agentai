e56ab1a779e7e687143ad18b1eab3fb674d3b78f
Matt Cannon <mttcannon@gmail.com>
2024-03-07 07:00:13 -0800

    adding new files - can answer population questions

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 from llama_index.core.query_engine import PandasQueryEngine
-from prompts import new_prompt, instruction_str
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+
+
 
 
 load_dotenv()
@@ -13,4 +19,19 @@ population_df = pd.read_csv(population_path)
 population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
 
 population_query_engine.update_prompts({"pandas_prompt": new_prompt})
-population_query_engine.query("what is the population of canada")
+
+tools = [
+    note_engine,
+    QueryEngineTool(query_engine=population_query_engine, metadata=ToolMetadata(
+        name="population_data",
+        description="this gives information of the world population and demographics"
+        ),
+    ),
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+while(prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
--- prompts.py
+++ prompts.py
@@ -20,3 +20,6 @@ new_prompt = PromptTemplate(
 
     Expression: """
 )
+
+context ="""Purpose: The primary role of this agent is to assist users by providing accurate
+            information about world population statistics and details about a country."""
